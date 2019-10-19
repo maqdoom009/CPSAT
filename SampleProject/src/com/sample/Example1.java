b2b392578477ba4c1f33b29b6a180b8b55d05fba
Md Junaid <Md Junaid@DESKTOP-TF80URS>
2019-10-19 14:19:33 -0400

    introduce yourself

---
+++ SampleProject/src/com/sample/Example1.java
@@ -0,0 +1,18 @@
+package com.sample;
+
+import java.util.Scanner;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String s1 = sc.nextLine();
+		System.out.println("How are you "+ s1+" ?");
+		String s2 = sc.nextLine();
+		System.out.println("That's nice!");
+		
+		
+	}
+
+}
